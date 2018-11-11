bc32c3e35c304923d7377f3536d07185733b7471
Ka Woong Hyun <kwh950724@gmail.com>
2018-11-11 22:33:18 +0900

    Modify tracking algorithm and front detect dist
    algorithm (detect nearest segment only)

--- scripts/tracking_action.py
+++ scripts/tracking_action.py
@@ -31,15 +31,15 @@ class tracking:
                 self.max_speed = rospy.get_param("/car_tracking/max_speed")
                 self.speed = 0
 		self.steer = 0
-		self.wall_width = rospy.get_param("/car_tracking/wall_width")
-		#self.car_width = rospy.get_param("/car_tracking/car_width")
+		self.temp_x = 777
 		self.wall_dist = rospy.get_param("/car_tracking/wall_dist") # distance of platform & wall desired
 		self.car_dist = rospy.get_param("/car_tracking/car_dist")   # distance of platform & car desired
-
+		
 		self.start_flag = False
 		self.finish_flag = False
 
-
+		self.wall_flag = False
+		
 	def target_lane_feedback_cb(self, feedback):
 		self.steer = feedback.tracking_feedback
 
@@ -67,76 +67,98 @@ class tracking:
 
 
 	def obstacles_cb(self, data):
+		
+		
 		if(self.start_flag == True):
-			self.client.wait_for_server()
-			self.client.send_goal(goal)
-
-			self.target_segment_center = Point(100,100,0)
-			# width of object 
-			self.detected_width = math.sqrt((self.segment.last_point.y - self.segment.first_point.y)**2 + (self.segment.last_point.x - self.segment.first_point.x)**2)
-			# distance of object (foucus distance)
-			self.detected_dist = math.sqrt(self.target_segment_center.x**2 + self.target_segment_center.y**2)
-			segment_center_point = data.segments
-			#search nearest segments		
-			for segment in segment_center_point:
-				distance = math.sqrt(((segment.last_point.x+segment.first_point.x)/2)**2 + ((segment.last_point.y+segment.first_point.y)/2)**2)
-				if (self.detected_dist> distance):
-					self.target_segment_center = Point(((segment.last_point.x+segment.first_point.x)/2),((segment.last_point.y+segment.first_point.y)/2),0)
 			
-			print("Target Obstacle : " + str(self.target_segment_center))
-			print("Target distance : " + str(self.S_now))
-			acker_data = AckermannDriveStamped()
-			#speed, steering
-			# detect the wall &  stop in a desired distance
-			if (abs(self.detected_width - self.wall_width)<0.5 and detected_dist< self.wall_dist):
-				print("wall is detected!")
-				self.client.cancel_goal()
-				acker_data.drive.speed = 0
-				acker_data.drive.steering_angle = 0
-				self.pub.publish(acker_data)
+			for segment in data.segments:
+				nearest_x = segment.first_point.x
+				if nearest_x >= segment.first_point.x:
+					nearest_x = segment.first_point.x
+				if nearest_x >= segment.last_point.x:
+					nearest_x = segment.last_point.x
+				# instant difference of x
+				# TODO: change 1.0 value to launch parameter!!!!
+				if(nearest_x - self.temp_x > 1.0):
+					rospy.loginfo('wall detected')
+					self.wall_flag = True
 
-				self.finish_flag = True
-			# detect nothing & speed is constant
-			elif(self.target_segment_center.x == 0):
-				print("detect nothing!")
-				acker_date.drive.speed = 3
-			# detect the object (including car) & control speed
-			else:
-				print("object is detected & chases!")
-				self.speed_t = self.speed + (self.detected_dist-self.car_dist)/self.HZ  #target speed by 'pi' controller
-				if (self.car_dist > self.detected_dist + 0.5):
-					print("deceleration")
+			self.temp_x = nearest_x
+			acker_data = AckermannDriveStamped() 
+			# wall is not detected	
+			if(self.wall_flag == False):
+				# searching for nearest x point for detecting wall		
+ 				#speed, steering
+	
+				# detect nothing & speed is constant
+				if(nearest_x == 0):
+					rospy.loginfo("detect nothing!")
+					acker_date.drive.speed = 3
+				# detect the object (including car) & control speed
+				else:
+					
+					rospy.loginfo("car is detected & chases!")
+					self.speed_t = self.speed + (nearest_x-self.car_dist)/self.HZ  #target speed by 'pi' controller
+					if (self.car_dist > nearest_x + 0.5):
+						rospy.loginfo("deceleration")
+						self.speed = self.speed_t - self.speed_error*100
+						self.before_speed = self.speed # define speed before a 1 func
+						self.brake = self.brake_unit
+					elif(self.car_dist + 0.5 < nearest_x):
+						rospy.loginfo("acceleration")
+						self.speed = self.speed_t + self.speed_error
+						self.before_speed = self.speed
+						self.brake = 0
+					elif(abs(nearest_x - self.car_dist) <= 0.5):
+						rospy.loginfo("const speed")
+						self.speed = self.before_speed
+						self.brake = 0
+
+					if(self.speed > self.max_speed):
+					    rospy.loginfo("speed reached max_...")
+					    self.speed = self.max_speed
+					elif(self.speed < 0):
+					    rospy.loginfo("speed is less than 0!!")
+					    self.speed = 0
+			else:	                                                                      
+				rospy.loginfo("wall is detected & approaching to the wall")
+				self.speed_t = self.speed + (nearest_x-self.wall_dist)/self.HZ  #target speed by 'pi' controller
+				
+				# optimizing 1.5 value, change to launch parameter!!!
+				if (self.wall_dist+1.5> nearest_x ):
+					rospy.loginfo("deceleration")
 					self.speed = self.speed_t - self.speed_error*100
 					self.before_speed = self.speed # define speed before a 1 func
 					self.brake = self.brake_unit
-				elif(self.car_dist + 0.5 < self.detected_dist):
-					print("acceleration")
+				elif(self.wall_dist+1.5 < nearest_x):
+					rospy.loginfo("acceleration")
 					self.speed = self.speed_t + self.speed_error
 					self.before_speed = self.speed
 					self.brake = 0
-				elif(abs(self.detected_dist - self.car_dist) <= 0.5):
-					print("const speed")
-					self.speed = self.before_speed
+				elif(abs(nearest_x - self.wall_dist) <= 0.5):
+					rospy.loginfo("finish!")
+					self.speed = 0
 					self.brake = 0
+					self.client.cancel_goal()
+					self.finish_flag = True
 
 				if(self.speed > self.max_speed):
-				    print("speed reached max_...")
+				    rospy.loginfo("speed reached max_...")
 				    self.speed = self.max_speed
 				elif(self.speed < 0):
-				    print("speed is less than 0!!")
+				    rospy.loginfo("speed is less than 0!!")
 				    self.speed = 0
-
-
+						
 			#safety action
-			if(self.detected_dist <0.5):
+			if(nearest_x < 1.0):
 				acker_data.drive.speed=0
 			
 			acker_data.drive.speed = int(self.speed)
 			acker_data.drive.brake = int(self.brake)		
 			acker_data.drive.steering_angle = self.steer #feedback steering data
-			print("speed : " + str(acker_data.drive.speed))
-			print("brake : " + str(acker_data.drive.brake))
-			print("steering : " + str(acker_data.drive.steering_angle))	
+			rospy.loginfo("speed : " + str(acker_data.drive.speed))
+			rospy.loginfo("brake : " + str(acker_data.drive.brake))
+			rospy.loginfo("steering : " + str(acker_data.drive.steering_angle))	
 
 			if self.finish_flag == False:
 				self.pub.publish(acker_data)
@@ -148,7 +170,7 @@ if __name__ == '__main__':
 		rospy.spin()	
 		
 	except rospy.ROSInterruptException:
-		print(error)
+		rospy.loginfo(error)
 		pass			
 				
 	
