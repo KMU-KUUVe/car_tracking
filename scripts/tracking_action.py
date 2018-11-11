2ce07879d1217e0ebc87d48ba5aeab69f7ad9a30
Jiwon Park <qwes8873@gmail.com>
2018-11-11 14:15:22 +0900

    Fix bug(make start_flag and finish_flag)

--- scripts/tracking_action.py
+++ scripts/tracking_action.py
@@ -21,9 +21,9 @@ class tracking:
 		self.goal = car_trackingGoal()
 
 		# Server
-		self.server = actionlib.SimpleActionServer('car_tracking', MissionPlannerAction, execute_cb=execute_cb, auto_start=False)
-		self.result = MissionPlannerResult()
-		
+		self.server = actionlib.SimpleActionServer('car_tracking', MissionPlannerAction, execute_cb=self.execute_cb, auto_start=False)
+		self.server.start()
+
 		self.HZ = rospy.get_param("/car_tracking/HZ")  # frequency of Ros node
 		self.speed_error = rospy.get_param("/car_tracking/speed_error")
                 self.brake_unit = rospy.get_param("/car_tracking/brake_unit")
@@ -34,8 +34,14 @@ class tracking:
 		#self.car_width = rospy.get_param("/car_tracking/car_width")
 		self.wall_dist = rospy.get_param("/car_tracking/wall_dist") # distance of platform & wall desired
 		self.car_dist = rospy.get_param("/car_tracking/car_dist")   # distance of platform & car desired
+
+		self.start_flag = False
+		self.finish_flag = False
+
+
 	def target_lane_feedback_cb(self, feedback):
 		self.steer = self.feedback.tracking_feedback
+
 
 	# LiDAR Algorithm Start
 	def execute_cb(self, goal):
@@ -46,76 +52,93 @@ class tracking:
 		# run algotihm
 		self.sub = rospy.Subscriber('raw_obstacles', Obstacles, self.obstacles_cb)
 
-	def obstacles_cb(self, data
-		self.client.wait_for_server()
-		self.client.send_goal(goal)
+		result = MissionPlannerResult()
 
-		self.target_segment_center = Point(100,100,0)
-		# width of object 
-		self.detected_width = math.sqrt((self.segment.last_point.y - self.segment.first_point.y)**2 + (self.segment.last_point.x - self.segment.first_point.x)**2)
-		# distance of object (foucus distance)
-		self.detected_dist = math.sqrt(self.target_segment_center.x**2 + self.target_segment_center.y**2)
-		segment_center_point = data.segments
-		#search nearest segments		
-		for segment in segment_center_point:
-			distance = math.sqrt(((segment.last_point.x+segment.first_point.x)/2)**2 + ((segment.last_point.y+segment.first_point.y)/2)**2)
-			if (self.detected_dist> distance):
-				self.target_segment_center = Point(((segment.last_point.x+segment.first_point.x)/2),((segment.last_point.y+segment.first_point.y)/2),0)
-		
-		print("Target Obstacle : " + str(self.target_segment_center))
-		print("Target distance : " + str(self.S_now))
-		acker_data = AckermannDriveStamped()
-		#speed, steering
-		# detect the wall &  stop in a desired distance
-		if (abs(self.detected_width - self.wall_width)<0.5 && detected_dist< self.wall_dist):
-			print("wall is detected!")
-			self.client.cancel_goal()
-			acker_data.drive.speed = 0
-			acker_data.drive.steering_angle = 0
-			self.pub.publish(acker_data)
-			self.server.set_succeeded(self.result)
-		# detect nothing & speed is constant
-		elif(self.target_segment_center.x == 0):
-			print("detect nothing!")
-			acker_date.drive.speed = 3
-		# detect the object (including car) & control speed
-		else:
-			print("object is detected & chases!")
-			self.speed_t = self.speed + (self.detected_dist-self.car_dist)/self.HZ  #target speed by 'pi' controller
-			if (self.car_dist > self.detected_dist + 0.5):
-                                print("deceleration")
-				self.speed = self.speed_t - self.speed_error*100
-                                self.before_speed = self.speed # define speed before a 1 func
-				self.brake = self.brake_unit
-                        elif(self.car_dist + 0.5 < self.detected_dist):
-                                print("acceleration")
-				self.speed = self.speed_t + self.speed_error
-				self.before_speed = self.speed
-                                self.brake = 0
-                        elif(abs(self.detected_dist - self.car_dist) <= 0.5):
-                                print("const speed")
-				self.speed = before_speed
-                                self.brake = 0
+		self.start_flag = True	
 
-                        if(self.speed > self.max_speed):
-			    print("speed reached max_...")
-                            self.speed = self.max_speed
-                        elif(self.speed < 0):
-                            print("speed is less than 0!!")
-                            self.speed = 0
+		r = rospy.Rate(100)
+		while not rospy.is_shutdown():
+			if(self.finish_flag == True):
+				self.start_flag = False
+				self.server.set_succeeded(result)	
+				break
+			r.sleep()
 
 
-		#safety action
-		if(self.detected_dist <0.5)
-			acker_data.drive.speed=0
-		
-		acker_data.drive.speed = int(self.speed)
-		acker_data.drive.brake = int(self.brake)		
-		acker_data.drive.steering_angle = self.steer #feedback steering data
-		print("speed : " + str(acker_data.drive.speed))
-		print("brake : " + str(acker_data.drive.brake))
-		print("steering : " + str(acker_data.drive.steering_angle))	
-		self.pub.publish(acker_data)
+	def obstacles_cb(self, data):
+		if(self.start_flag == True):
+			self.client.wait_for_server()
+			self.client.send_goal(goal)
+
+			self.target_segment_center = Point(100,100,0)
+			# width of object 
+			self.detected_width = math.sqrt((self.segment.last_point.y - self.segment.first_point.y)**2 + (self.segment.last_point.x - self.segment.first_point.x)**2)
+			# distance of object (foucus distance)
+			self.detected_dist = math.sqrt(self.target_segment_center.x**2 + self.target_segment_center.y**2)
+			segment_center_point = data.segments
+			#search nearest segments		
+			for segment in segment_center_point:
+				distance = math.sqrt(((segment.last_point.x+segment.first_point.x)/2)**2 + ((segment.last_point.y+segment.first_point.y)/2)**2)
+				if (self.detected_dist> distance):
+					self.target_segment_center = Point(((segment.last_point.x+segment.first_point.x)/2),((segment.last_point.y+segment.first_point.y)/2),0)
+			
+			print("Target Obstacle : " + str(self.target_segment_center))
+			print("Target distance : " + str(self.S_now))
+			acker_data = AckermannDriveStamped()
+			#speed, steering
+			# detect the wall &  stop in a desired distance
+			if (abs(self.detected_width - self.wall_width)<0.5 && detected_dist< self.wall_dist):
+				self.finish_flag = True
+				
+				print("wall is detected!")
+				self.client.cancel_goal()
+				acker_data.drive.speed = 0
+				acker_data.drive.steering_angle = 0
+				self.pub.publish(acker_data)
+			# detect nothing & speed is constant
+			elif(self.target_segment_center.x == 0):
+				print("detect nothing!")
+				acker_date.drive.speed = 3
+			# detect the object (including car) & control speed
+			else:
+				print("object is detected & chases!")
+				self.speed_t = self.speed + (self.detected_dist-self.car_dist)/self.HZ  #target speed by 'pi' controller
+				if (self.car_dist > self.detected_dist + 0.5):
+					print("deceleration")
+					self.speed = self.speed_t - self.speed_error*100
+					self.before_speed = self.speed # define speed before a 1 func
+					self.brake = self.brake_unit
+				elif(self.car_dist + 0.5 < self.detected_dist):
+					print("acceleration")
+					self.speed = self.speed_t + self.speed_error
+					self.before_speed = self.speed
+					self.brake = 0
+				elif(abs(self.detected_dist - self.car_dist) <= 0.5):
+					print("const speed")
+					self.speed = before_speed
+					self.brake = 0
+
+				if(self.speed > self.max_speed):
+				    print("speed reached max_...")
+				    self.speed = self.max_speed
+				elif(self.speed < 0):
+				    print("speed is less than 0!!")
+				    self.speed = 0
+
+
+			#safety action
+			if(self.detected_dist <0.5)
+				acker_data.drive.speed=0
+			
+			acker_data.drive.speed = int(self.speed)
+			acker_data.drive.brake = int(self.brake)		
+			acker_data.drive.steering_angle = self.steer #feedback steering data
+			print("speed : " + str(acker_data.drive.speed))
+			print("brake : " + str(acker_data.drive.brake))
+			print("steering : " + str(acker_data.drive.steering_angle))	
+
+			if self.finish_flag == False:
+				self.pub.publish(acker_data)
 
 
 if __name__ == '__main__':
